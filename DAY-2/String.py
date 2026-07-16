db46aea4e95d8fb42a461d0167c330f78c6d31da
Deepak Verma <deepak24fff@gmail.com>
2026-07-16 22:40:30 +0530

    String In python

---
+++ DAY-2/String.py
@@ -0,0 +1,18 @@
+word = "I am Deepak"
+# print(len(word)) # 11
+
+# Print Deepak by slicing
+print(word[5:])    # Deepak
+print(word[-9:-6]) # am
+
+
+
+# String Format
+a = 10
+b = 5
+sum = a + b
+print("Sum of {} and {} is {}".format(a,b,sum))
+
+# OR
+
+print(f"Sum of {a} and {b} is {sum}")
