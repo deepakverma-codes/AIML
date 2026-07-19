ca14da90502bd781b6bd789b2fb0951458651581
Deepak Verma <deepak24fff@gmail.com>
2026-07-19 19:42:11 +0530

    Revised list methods and use of loop in list

--- DAY-2/list.py
+++ DAY-2/list.py
@@ -5,3 +5,29 @@ print(type(list))
 # List are mutable
 list[0] = 00
 print(list)
+
+#-------------Methods---------------------#
+l = [23,1,55,8,98]
+l.sort(reverse=True)
+print(l)
+
+l.insert(2, 999)
+print(l)
+
+l.reverse()
+print(l)
+
+
+#-------------Loop In list-------------#
+marks = [34,78,3,88,65]
+idx = 0
+found = False
+
+# Check index of 88
+for val in marks:
+    if val == 88:
+        found = True
+        break
+    idx+=1 
+if found:
+    print(f"Index of 88 is {idx}")
