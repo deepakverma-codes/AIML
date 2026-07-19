762a889628cec547ff1c6134af77f411f91b90a3
Deepak Verma <deepak24fff@gmail.com>
2026-07-19 20:53:58 +0530

    Revised tuple and its methods

---
+++ DAY-2/tuple.py
@@ -0,0 +1,12 @@
+tuple = (1,23,3,3,3, 4,34,87)
+
+# Sum of all element by loop
+sum = 0
+for item in tuple:
+    sum += item
+print(sum)
+
+
+#----------Methods------------#
+print(tuple.index(3))
+print(tuple.count(3))
